a94fea3e9f57617b8ade838ba8336edf380fb50e
gmazanashvili <gmazanashvili@SSAGMARZANASHVI>
2017-07-18 23:34:48 -0700

    Create Repository class

---
+++ Generic Repository/Generic Repository/Repository/Contracts/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using Generic_Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Repository
+{
+    public interface IRepository<TEntity> : IQueryable<TEntity>, IDisposable where TEntity : BaseEntity
+    {
+        TEntity Get(int ID);
+        TEntity Get(Expression<Func<TEntity, bool>> predicate);
+
+        IQueryable<TEntity> GetAll();
+
+        IQueryable<TEntity> GetRange(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+        void Remove(int entityID);
+    }
+}
